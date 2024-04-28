8e1f5fa872afc89a5396f01853d9bbf3db499c65
code8-cool <136864069+code8-cool@users.noreply.github.com>
2024-04-27 19:18:55 -0700

    Add files via upload

---
+++ chair.js
@@ -0,0 +1,67 @@
+img = "dog_cat.jpg";
+status = "";
+objects = [];
+
+function preload() {
+img = loadImage('dog_cat.jpg');
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult (error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results)
+    objects = results;
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocssd', modelLoaded );
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(img, gotResult);
+       
+
+         for (i = 0; i < objects.length; i++) 
+         {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label +  " " + percent + "%" , objects[i].x + 15, objects[i].y + 15 );
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            
+         }
+
+    }
+
+
+
+}
